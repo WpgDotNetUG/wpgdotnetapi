1eab7d7224e86fb0180b2f3bc55b35b598fe8f7d
Quinn Wilson <git@midword.org>
2016-04-21 10:23:44 -0500

    issue 500 error directly instead of throwing exception

--- slack.fsx
+++ slack.fsx
@@ -1,38 +1,40 @@
 #r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "packages/Suave/lib/net40/Suave.dll"
 #r "packages/FSharpx.Extras/lib/40/FSharpx.Extras.dll"
-#load "wpgdotnet.fsx"
 
 open System
 open FSharp.Data
 open Suave
-open Suave.Successful // for OK-result
-open Suave.Http 
-open FSharpx.Nullable
-open wpgdotnet
+open Suave.ServerErrors
+open Suave.Successful 
 
-let inline (|??) a b = match a with 
+let inline (|??) a b = match a with  // for pulling values out of non f# code.. that might be null
                         | null -> b
                         | _ -> a
 
 module Slack = 
-    let INTERNAL_ERROR ctx = 
-        raise (wpgdotnet.InternalException "Internal Error")
 
     type SlackMessage = JsonProvider<""" { "ok":"false", "error":"not authorized" } """>
-    let invite ctx email   =
+
+    let asyncError (ctx:HttpContext) (msg:string) = 
+        async{
+            let formattedMessage = sprintf """{"errorMessage":"%s"}""" msg
+            printfn "error => %s" formattedMessage
+            return! INTERNAL_ERROR formattedMessage ctx
+        }
+
+    let invite (ctx : HttpContext) email   =
         async {
             let org = (Environment.GetEnvironmentVariable("SLACK_ORG")) |?? "wpgdotnet" // don't die if environment variable not set
             let token =  Environment.GetEnvironmentVariable("SLACK_TOKEN") 
             let url = sprintf "https://%s.slack.com/api/users.admin.invite?token=%s" org token
             let! apiResponse = Http.AsyncRequestString(url, body = FormValues ["email", email])
-
             let slackResponse = SlackMessage.Parse(apiResponse)
-            if not slackResponse.Ok  then
-                INTERNAL_ERROR (slackResponse.Error) ctx
-            return! OK (apiResponse) ctx
+            match slackResponse.Ok with 
+            | false -> return! (asyncError ctx slackResponse.Error)
+            | true ->return! OK (apiResponse) ctx
         }
 
     let signUp (ctx:HttpContext) = 
         ctx.request.formData "email" 
-        |> FSharpx.Choice.choice (invite ctx) INTERNAL_ERROR 
+        |> FSharpx.Choice.choice (invite ctx) (fun i -> asyncError ctx "Missing email")
